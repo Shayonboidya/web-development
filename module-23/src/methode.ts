748c1a363b3f97a353572b04dba0c359cbeeb5f7
Shayon Boidya <shayonboidya11@gmail.com>
2026-09-11 00:07:54 +0600

    update module-23

--- module-23/src/methode.ts
+++ module-23/src/methode.ts
@@ -20,5 +20,5 @@ class Student {
     
 }
 
-const sh = new Student("Shayon", 21,"shayon@11",90);
-console.log(sh.getINfo());
+const sh = new Student("Shayon", 21,"shayon@11gamil.com",90);
+sh.getINfo();
